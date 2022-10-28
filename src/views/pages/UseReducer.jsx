4c177b77a2fea40229d9892fb5f0764244c7a9f1
Jefferson Rodrigues <jefferson.rodrigues@fitbank.com.br>
2022-10-28 13:59:39 -0300

    Aprendendo sobre useReducer

--- src/views/pages/UseReducer.jsx
+++ src/views/pages/UseReducer.jsx
@@ -1,14 +1,53 @@
+import { useReducer } from "react";
 import SectionTitle from "../../components/layout/SectionTitle";
 import Title from "../../components/layout/Title";
 
+const initialState = {
+  other: "...",
+  cart: [],
+  products: [],
+  //foco
+  number: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add2ToNumber":
+      return { ...state, number: state.number + 2 };
+    case "login":
+      return {
+        ...state,
+        user: {
+          name: "Jefferson",
+        },
+      };
+    default:
+      return state;
+  }
+}
+
 const UseReducer = (props) => {
+  const [state, exec] = useReducer(reducer, initialState);
+
   return (
     <div className="UseReducer">
       <Title
         title={"UseReducer"}
-        subtitle={"Uma outra forma de ter estado em componentes"}
+        subtitle={"Uma outra forma de ter estado em componentes funcionais"}
       />
       <SectionTitle title={"Atividade 01"} />
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem usuário</span>
+        )}
+        <span className="text">{state.number}</span>
+
+        <button className="btn" onClick={() => exec({ type: "add2ToNumber" })}>
+          +2
+        </button>
+      </div>
 
       <SectionTitle title={"Atividade 02"} />
     </div>
